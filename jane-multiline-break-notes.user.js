8d409da2fd80e2b634db89c1ac7c0d5e8fae13b9
werpswerps <werpswerps@proton.me>
2026-03-20 13:06:22 -0700

    Add author metadata to user script

--- jane-multiline-break-notes.user.js
+++ jane-multiline-break-notes.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name         JaneApp - Multiline break notes
 // @version      1.1
+// @author       werpswerps
 // @match        https://*.janeapp.com/admin*
 // @description  Preserves line breaks when adding notes to a break on the Jane schedule. Without this, all lines run together into a single line.
 // @grant        none
