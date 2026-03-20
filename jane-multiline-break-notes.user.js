1202cc86e05e1b6de6f6b4064419515af49da6a9
werpswerps <werpswerps@proton.me>
2026-03-20 12:05:07 -0700

    Reordered metadata for cross-script consistency

--- jane-multiline-break-notes.user.js
+++ jane-multiline-break-notes.user.js
@@ -1,7 +1,9 @@
 // ==UserScript==
-// @name        JaneApp - Multiline break notes
-// @description Preserves line breaks when adding notes to a break on the Jane schedule. Without this, all lines run together into a single line.
-// @match       https://*.janeapp.com/admin*
+// @name         JaneApp - Multiline break notes
+// @version      1.1
+// @match        https://*.janeapp.com/admin*
+// @description  Preserves line breaks when adding notes to a break on the Jane schedule. Without this, all lines run together into a single line.
+// @grant        none
 // ==/UserScript==
 
 const style = document.createElement('style');
